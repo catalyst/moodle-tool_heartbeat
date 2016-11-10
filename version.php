c9d2c60f9737c01c524efafa07e59f20f5b587e0
Brendan Heywood <brendan@catalyst-au.net>
2016-11-10 16:49:14 +1100

    Bump version

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016101700;
-$plugin->release   = 2016101700; // Match release exactly to version.
+$plugin->version   = 2016111000;
+$plugin->release   = 2016111000; // Match release exactly to version.
 $plugin->requires  = 2014050800;
 $plugin->component = 'tool_heartbeat';
 $plugin->maturity  = MATURITY_STABLE;
