e61c750067d8ff315211f1fb1ef77392067300bd
Brendan Heywood <brendan@catalyst-au.net>
2017-10-12 16:45:17 +1100

    Version bump

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017060700;
-$plugin->release   = 2017051600; // Match release exactly to version.
+$plugin->version   = 2017101200;
+$plugin->release   = 2017101200; // Match release exactly to version.
 $plugin->requires  = 2012120311; // Deep support going back to 2.4
 $plugin->component = 'tool_heartbeat';
 $plugin->maturity  = MATURITY_STABLE;
