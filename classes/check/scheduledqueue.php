befe9ef6a223f98d6a0e9ce3549849d5403bb6d8
Michael Reyes <michaelreyes@catca-laptop2452.catalyst-ca.net>
2026-05-20 15:42:59 -0400

    Issue #247: Ignore scheduled task overdue if plugin component is disabled

--- classes/check/scheduledqueue.php
+++ classes/check/scheduledqueue.php
@@ -19,6 +19,7 @@ namespace tool_heartbeat\check;
 use action_link;
 use core\check\check;
 use core\check\result;
+use core_plugin_manager;
 use moodle_url;
 
 /**
@@ -43,6 +44,7 @@ class scheduledqueue extends check {
      */
     public function get_result(): result {
         $now = time();
+        $pluginmanager = core_plugin_manager::instance();
 
         // Use the task manager API so that $CFG->scheduled_tasks config overrides
         // (which can force-disable or force-enable tasks) are respected.
@@ -53,6 +55,14 @@ class scheduledqueue extends check {
             if ($task->get_disabled()) {
                 continue;
             }
+
+            // Mirror core task dispatch behaviour: if the component plugin is
+            // disabled, skip this task unless it is explicitly allowed to run.
+            $plugininfo = $pluginmanager->get_plugin_info($task->get_component());
+            if ($plugininfo && $plugininfo->is_enabled() === false && !$task->get_run_if_component_disabled()) {
+                continue;
+            }
+
             $age = $now - $task->get_next_run_time();
             if ($age > 0) {
                 $overdue[] = (object)[
